c46feb71e0e9d5b8543a91596980df9f2497de37
Eric Parsonnet <e.parsonnet@berkeley.edu>
2021-10-27 23:50:08 -0700

    fix

--- seeq/addons/clustering/app/clusterApp/ui.py
+++ seeq/addons/clustering/app/clusterApp/ui.py
@@ -78,10 +78,10 @@ def clusterUnsupervised(app, buttons, signals, minClusterSize, exactBox, percent
     #for cluster extent looks at .extent_scalar
     app.extent_scalar = float(clusterExtent.value) 
     
-    _wrapUp(app, checksum, basename, timeOfRun)
+    _wrapUp(app, checksum, basename, timeOfRun, closeButton)
     return 
 
-def _wrapUp(app, checksum, basename, timeOfRun):
+def _wrapUp(app, checksum, basename, timeOfRun, closeButton):
     print("\rPushing Conditions...")
     clear()
     app.push_cluster_formulas(checksum, basename, timeOfRun)
@@ -236,7 +236,7 @@ def clusterSupervised(app, buttons, xsignal, ysignal, clusterExtent, datadf, ind
     
     app.extent_scalar = float(clusterExtent.value) #for cluster extent looks at self.extent_scalar
     
-    _wrapUp(app, checksum, basename, timeOfRun)
+    _wrapUp(app, checksum, basename, timeOfRun, closeButton)
     return
 
 def find_true_points_in_selection_boundary(indexofselection, check_points, hist_grid_points):
